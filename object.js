11f1fafcb0359837ed91107f7232de044e12220d
atiullah23 <ashmirvlog786@gmail.com>
2025-10-26 18:35:48 +0545

    Add new code

--- object.js
+++ object.js
@@ -1,18 +1,9 @@
-class Toyotacar{
-    constructor(brand,mileage){
-        console.log("Creating a new  car");
-        this.brand=brand;
-        this.mileage=mileage;
-    }
-    start(){
-        console.log("start");
-    }
-    stop(){
-        console.log("stop");
+class parent {
+    hello(){
+        console.log("Hello");
     }
 }
 
-let Fortuner=new Toyotacar("fortuner",10);
-console.log(Fortuner);
-let Tata= new Toyotacar("Kia",20);
-console.log(Tata);
+class Child extends parent{}
+
+let obj=new Child();
