9ee61984d864ca30a6dc259a496217ee37396ce6
atiullah23 <ashmirvlog786@gmail.com>
2025-10-26 17:16:16 +0545

    Add some code

--- object.js
+++ object.js
@@ -1,7 +1,19 @@
-const student={
-    fullName:"Er Ashmir Khan",
-    marks:94.4,
-    printMarks:function(){
-        console.log("marks=",this.marks);
-    },
+// const student={
+//     fullName:"Er Ashmir Khan",
+//     marks:94.4,
+//     printMarks:function(){
+//         console.log("marks=",this.marks);
+//     },
+// };
+
+const employee={
+calcTax(){
+    console.log("The empolyee Tas is 10%");
+}
 };
+
+const karanArjun={
+    salary:50000,
+}
+
+karanArjun.__proto__=employee;
